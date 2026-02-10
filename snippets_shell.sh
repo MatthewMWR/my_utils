0c7314645c6925c8bcd5ef0423ecdc4e01c90aab
MatthewMWR <void@example.com>
2026-02-10 21:18:31 +0000

    general sync

--- snippets_shell.sh
+++ snippets_shell.sh
@@ -1,2 +1,3 @@
 ## Reliably delete a resources group which contains a VM
-az group delete --name RG_NAME --force-deletion-types Microsoft.Compute/VirtualMachines
+RG_NAME="demo_rg_8254"
+az group delete --name "$RG_NAME" --force-deletion-types Microsoft.Compute/VirtualMachines
